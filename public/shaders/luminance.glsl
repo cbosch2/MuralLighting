68b7daba380a95235a23bc50616a0d3abb001bb7
Manvir05 <manvir.kaur@upc.edu>
2025-01-22 11:24:19 +0000

    test tonemappingluminance

--- public/shaders/luminance.glsl
+++ public/shaders/luminance.glsl
@@ -1,14 +1,24 @@
 #include <ColorOps>
 
-uniform float Max_L; //maximum luminance of the scene or which luminance will map to white
+uniform sampler2D colorMap;    // Magma colormap (treated as 2D texture)
+uniform float Max_L;           // Maximum luminance of the scene or which luminance maps to white
 
 vec3 CustomToneMapping( vec3 color ) {
-    //obtain luminace
-    vec3 XYZ = convert_RGB2XYZ(color); 
+    // Obtain luminance from input color
+    vec3 XYZ = convert_RGB2XYZ(color);
     float luminance = XYZ.y;
 
-    //normalize luminance
-    float norm_lum = clamp(luminance/Max_L, 0.f, 1.f);
+    // Normalize luminance
+    float norm_lum = clamp(luminance / Max_L, 0.0, 1.0);
 
-    return vec3(norm_lum);
+    // Map normalized luminance to the colormap (using a fixed vertical coordinate of 0.5)
+    vec3 mappedColor = texture2D(colorMap, vec2(norm_lum, 0.5)).rgb;
+
+    return mappedColor;
 }
+
+
+
+
+
+
--- public/toneMappingLuminance.js
+++ public/toneMappingLuminance.js
@@ -5,8 +5,11 @@ var shaderCode = await readTextFile("shaders/luminance.glsl");
 
 var shaderParameters = {
     "Max_L": { min: 0.0, max: 0.001, value: 0.0001, name: "Max Luminance" },
+
 };
 
 const toneMappingLuminance = new ToneMappingFunction("Luminance", shaderCode, shaderParameters);
 
 export default toneMappingLuminance;
+
+
