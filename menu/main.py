5c89a24bc4b5bf91ce948fb7bf8a46a86a784523
juditmoral <judit.moral03@gmail.com>
2025-10-23 10:15:41 +0200

    load node and python at the same time

--- menu/main.py
+++ menu/main.py
@@ -1,6 +1,9 @@
-from nicegui import ui # pip install nicegui
+from nicegui import ui, app
+import os
 
+menu_path = os.path.dirname(__file__) 
 
+app.add_static_files('/menu', menu_path)
 
 selected_cards = [] # global list to control the selected ones
 
@@ -11,19 +14,31 @@ iframe_container = None
 def show_selected_images():
     if len(selected_cards) == 2:
         def format_exr(image_name):
-            # Get the folder name and file name
-            parts = image_name.rsplit('/', 1)  # ['folder', 'file.jpg']
-            folder = parts[0]                  # folder
+            # Remove '/menu/' if it is at the beginning
+            if image_name.startswith('/menu/'):
+                image_name = image_name[len('/menu/'):]
+            
+            # Get the folder and file name
+            parts = image_name.rsplit('/', 1)        # ['folder', 'file.jpg']
+            folder = parts[0]                        # folder
             file_name = parts[1].rsplit('.', 1)[0]  # name without extension
-            folder = folder.replace('+', '%2B')
-            return f"XII/{folder}/{file_name}.exr"   # new format
 
+            
+            folder = folder.replace('+', '%2B')
+
+            # Returns the final route for the viewer
+            return f"XII/{folder}/{file_name}.exr"
+
+        # Generates images for the viewer
         img1 = format_exr(selected_cards[0]["image"])
         img2 = format_exr(selected_cards[1]["image"])
+        
+        # Iframe URL
         url = f"http://127.0.0.1:3006/index.html?img1={img1}&img2={img2}"
     else:
         url = "http://127.0.0.1:3006/index.html"
 
+    # Returns the iframe with the generated URL
     return f'''
     <div class="w-full h-full">
         <iframe 
@@ -163,33 +178,33 @@ def main():
                     with ui.card():
                         ui.markdown(f'##### {D2}').classes(heading_classes)
                         with ui.row().classes('w-full'):
-                            card("natural/D2T1-pv2.jpg", [NATURAL, DD2, D2T1, ART_NONE], classes)
-                            card("natural/D2T2-pv2.jpg", [NATURAL, DD2, D2T2, ART_NONE], classes)
-                            card("natural/D2T3-pv2.jpg", [NATURAL, DD2, D2T3, ART_NONE], classes)
+                            card("/menu/natural/D2T1-pv2.jpg", [NATURAL, DD2, D2T1, ART_NONE], classes)
+                            card("/menu/natural/D2T2-pv2.jpg", [NATURAL, DD2, D2T2, ART_NONE], classes)
+                            card("/menu/natural/D2T3-pv2.jpg", [NATURAL, DD2, D2T3, ART_NONE], classes)
 
                     with ui.card():
                         ui.markdown(f'##### {D3}').classes(heading_classes)
                         with ui.row().classes('w-full'):
-                            card("natural/D3T1-pv2.jpg", [NATURAL, DD3, D3T1, ART_NONE], classes)
-                            card("natural/D3T2-pv2.jpg", [NATURAL, DD3, D3T2, ART_NONE], classes)
-                            card("natural/D3T3-pv2.jpg", [NATURAL, DD3, D3T3, ART_NONE], classes)
+                            card("/menu/natural/D3T1-pv2.jpg", [NATURAL, DD3, D3T1, ART_NONE], classes)
+                            card("/menu/natural/D3T2-pv2.jpg", [NATURAL, DD3, D3T2, ART_NONE], classes)
+                            card("/menu/natural/D3T3-pv2.jpg", [NATURAL, DD3, D3T3, ART_NONE], classes)
 
                     with ui.card():
                         ui.markdown(f'##### {D1}').classes(heading_classes)
                         with ui.row().classes('w-full'):
-                            card("natural/D1T1-pv2.jpg", [NATURAL, DD1, D1T1, ART_NONE], classes)
-                            card("natural/D1T2-pv2.jpg", [NATURAL, DD1, D1T2, ART_NONE], classes)
-                            card("natural/D1T3-pv2.jpg", [NATURAL, DD1, D1T3, ART_NONE], classes)
+                            card("/menu/natural/D1T1-pv2.jpg", [NATURAL, DD1, D1T1, ART_NONE], classes)
+                            card("/menu/natural/D1T2-pv2.jpg", [NATURAL, DD1, D1T2, ART_NONE], classes)
+                            card("/menu/natural/D1T3-pv2.jpg", [NATURAL, DD1, D1T3, ART_NONE], classes)
 
     with expansion(tabs, panels, "Artificial illumination", 'The images below were rendered with **no natural lighting**, with **different layouts** for the **candles** and **oil lamps**:'):
         with ui.column().classes('w-full'):
             with ui.row().classes('w-full justify-center items-center'):
                 classes = "min-w-[220px] min-h-[400px]"
-                card("artificial/C1-pv2.jpg", [NAT_NONE, ARTIFICIAL, C1], classes)
-                card("artificial/C2-pv2.jpg", [NAT_NONE, ARTIFICIAL, C2], classes)
-                card("artificial/C3-pv2.jpg", [NAT_NONE, ARTIFICIAL, C3], classes)
-                card("artificial/C4-pv2.jpg", [NAT_NONE, ARTIFICIAL, C4], classes)
-                card("artificial/C5-pv2.jpg", [NAT_NONE, ARTIFICIAL, C1, C2, C4], classes)
+                card("/menu/artificial/C1-pv2.jpg", [NAT_NONE, ARTIFICIAL, C1], classes)
+                card("/menu/artificial/C2-pv2.jpg", [NAT_NONE, ARTIFICIAL, C2], classes)
+                card("/menu/artificial/C3-pv2.jpg", [NAT_NONE, ARTIFICIAL, C3], classes)
+                card("/menu/artificial/C4-pv2.jpg", [NAT_NONE, ARTIFICIAL, C4], classes)
+                card("/menu/artificial/C5-pv2.jpg", [NAT_NONE, ARTIFICIAL, C1, C2, C4], classes)
 
 
     with expansion(tabs, panels, "Natural and artificial illumination", 'The images below were rendered with **natural lighting** and varying **artificial light sources**:'):
@@ -200,14 +215,14 @@ def main():
             with ui.card():
                 ui.markdown(f'##### {D2}').classes(heading_classes)
                 with ui.row().classes('w-full'):
-                    card("Natural+Artificial/D2T3-C2-pv2.jpg", [NATURAL, DD2, D2T3, ARTIFICIAL, C2], classes)
-                    card("Natural+Artificial/D2T3-C5-pv2.jpg", [NATURAL, DD2, D2T3, ARTIFICIAL, C1, C2, C4], classes)
+                    card("/menu/Natural+Artificial/D2T3-C2-pv2.jpg", [NATURAL, DD2, D2T3, ARTIFICIAL, C2], classes)
+                    card("/menu/Natural+Artificial/D2T3-C5-pv2.jpg", [NATURAL, DD2, D2T3, ARTIFICIAL, C1, C2, C4], classes)
 
             with ui.card():
                 ui.markdown(f'##### {D1}').classes(heading_classes)
                 with ui.row().classes('w-full'):
-                    card("Natural+Artificial/D1T3-C2-pv2.jpg", [NATURAL, DD1, D1T3, ARTIFICIAL, C2], classes)
-                    card("Natural+Artificial/D1T3-C5-pv2.jpg", [NATURAL, DD1, D1T3, ARTIFICIAL, C1, C2, C4], classes)
+                    card("/menu/Natural+Artificial/D1T3-C2-pv2.jpg", [NATURAL, DD1, D1T3, ARTIFICIAL, C2], classes)
+                    card("/menu/Natural+Artificial/D1T3-C5-pv2.jpg", [NATURAL, DD1, D1T3, ARTIFICIAL, C1, C2, C4], classes)
 
 
 
@@ -221,33 +236,33 @@ def main():
                 with ui.card():
                     ui.markdown(f'##### {D2}').classes(heading_classes)
                     with ui.row().classes('w-full'):
-                        card("natural/D2T1-pv2.jpg", [D2T1[2:]], classes)
-                        card("natural/D2T2-pv2.jpg", [D2T2[2:]], classes)
-                        card("natural/D2T3-pv2.jpg", [D2T3[2:]], classes)
+                        card("/menu/natural/D2T1-pv2.jpg", [D2T1[2:]], classes)
+                        card("/menu/natural/D2T2-pv2.jpg", [D2T2[2:]], classes)
+                        card("/menu/natural/D2T3-pv2.jpg", [D2T3[2:]], classes)
 
                 with ui.card():
                     ui.markdown(f'##### {D3}').classes(heading_classes)
                     with ui.row().classes('w-full'):
-                        card("natural/D3T1-pv2.jpg", [D3T1[2:]], classes)
-                        card("natural/D3T2-pv2.jpg", [D3T2[2:]], classes)
-                        card("natural/D3T3-pv2.jpg", [D3T3[2:]], classes)
+                        card("/menu/natural/D3T1-pv2.jpg", [D3T1[2:]], classes)
+                        card("/menu/natural/D3T2-pv2.jpg", [D3T2[2:]], classes)
+                        card("/menu/natural/D3T3-pv2.jpg", [D3T3[2:]], classes)
 
                 with ui.card():
                     ui.markdown(f'##### {D1}').classes(heading_classes)
                     with ui.row().classes('w-full'):
-                        card("natural/D1T1-pv2.jpg", [D1T1[2:]], classes)
-                        card("natural/D1T2-pv2.jpg", [D1T2[2:]], classes)
-                        card("natural/D1T3-pv2.jpg", [D1T3[2:]], classes)
+                        card("/menu/natural/D1T1-pv2.jpg", [D1T1[2:]], classes)
+                        card("/menu/natural/D1T2-pv2.jpg", [D1T2[2:]], classes)
+                        card("/menu/natural/D1T3-pv2.jpg", [D1T3[2:]], classes)
     
         with ui.card().classes('w-full'):
             ui.markdown('##### Artificial illumination')
             with ui.row().classes('w-full justify-center items-center'):
                 classes = "min-w-[180px] min-h-[300px]"
-                card("artificial/C1-pv2.jpg", [C1[2:]], classes)
-                card("artificial/C2-pv2.jpg", [C2[2:]], classes)
-                card("artificial/C3-pv2.jpg", [C3[2:]], classes)
-                card("artificial/C4-pv2.jpg", [C4[2:]], classes)
-                card("artificial/C5-pv2.jpg", [C1[2:], C2[2:], C4[2:]], classes)
+                card("/menu/artificial/C1-pv2.jpg", [C1[2:]], classes)
+                card("/menu/artificial/C2-pv2.jpg", [C2[2:]], classes)
+                card("/menu/artificial/C3-pv2.jpg", [C3[2:]], classes)
+                card("/menu/artificial/C4-pv2.jpg", [C4[2:]], classes)
+                card("/menu/artificial/C5-pv2.jpg", [C1[2:], C2[2:], C4[2:]], classes)
 
         with ui.card().classes('w-full'):
             ui.markdown('##### Natural & artificial illumination')
@@ -258,14 +273,14 @@ def main():
                 with ui.card():
                     ui.markdown(f'##### {D2}').classes(heading_classes)
                     with ui.row().classes('w-full'):
-                        card("Natural+Artificial/D2T3-C2-pv2.jpg", [D2T3[2:], C2[2:]], classes)
-                        card("Natural+Artificial/D2T3-C5-pv2.jpg", [D2T3[2:], C1[2:], C2[2:], C4[2:]], classes)
+                        card("/menu/Natural+Artificial/D2T3-C2-pv2.jpg", [D2T3[2:], C2[2:]], classes)
+                        card("/menu/Natural+Artificial/D2T3-C5-pv2.jpg", [D2T3[2:], C1[2:], C2[2:], C4[2:]], classes)
 
                 with ui.card():
                     ui.markdown(f'##### {D1}').classes(heading_classes)
                     with ui.row().classes('w-full'):
-                        card("Natural+Artificial/D1T3-C2-pv2.jpg", [D1T3[2:], C2[2:]], classes)
-                        card("Natural+Artificial/D1T3-C5-pv2.jpg", [D1T3[2:], C1[2:], C2[2:], C4[2:]], classes)
+                        card("/menu/Natural+Artificial/D1T3-C2-pv2.jpg", [D1T3[2:], C2[2:]], classes)
+                        card("/menu/Natural+Artificial/D1T3-C5-pv2.jpg", [D1T3[2:], C1[2:], C2[2:], C4[2:]], classes)
 
 
     with expansion(tabs, panels, "Interactive viewer",""):
--- webapp/app.js
+++ webapp/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const fs = require('fs')
 const path = require('path')
+const { execSync, spawn } = require('child_process')
 
 const app = express()
 
@@ -8,31 +9,53 @@ app.use(express.static(__dirname + '/public'))
 app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')))
 app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')))
 
+// Function to check and install nicegui if necessary 
+function ensureNiceguiInstalled() {
+  try {
+    execSync('python3 -m pip show nicegui', { stdio: 'ignore' })
+    
+  } catch (error) {
 
-////// List of images
-
-function getEXRFiles(basePath, dir = '', arrayOfFiles = []) {
-    const dirPath = basePath + dir;
-    const files = fs.readdirSync(dirPath);   // read directory contents
-
-    files.forEach(function(file) {
-        const fullPath = path.join(dirPath, file);  // full path
-        const relPath = dir + file;                 // relative path 
-        if (fs.statSync(fullPath).isDirectory())    // recurse if directory
-            arrayOfFiles = getEXRFiles(basePath, relPath + '/', arrayOfFiles);
-        else if (path.extname(file) === '.exr')     // add file to array if EXR
-            arrayOfFiles.push(relPath);
-    });
-
-    return arrayOfFiles;
+    execSync('pip3 install nicegui', { stdio: 'inherit' })
+  }
 }
 
-// Send images' filenames to the client
+// Start main.py
+function startPythonApp() {
+  // Absolute path to main.py inside the /menu folder
+  const mainPath = path.join(__dirname, '../menu/main.py')
+  
+
+  const pythonProcess = spawn('python3', [mainPath], { stdio: 'inherit' })
+  
+  
+}
+
+//List of .EXR images
+function getEXRFiles(basePath, dir = '', arrayOfFiles = []) {
+  const dirPath = basePath + dir
+  const files = fs.readdirSync(dirPath)
+
+  files.forEach(function (file) {
+    const fullPath = path.join(dirPath, file)
+    const relPath = dir + file
+    if (fs.statSync(fullPath).isDirectory())
+      arrayOfFiles = getEXRFiles(basePath, relPath + '/', arrayOfFiles)
+    else if (path.extname(file) === '.exr')
+      arrayOfFiles.push(relPath)
+  })
+
+  return arrayOfFiles
+}
+
 app.get('/images', (req, res) => {
-    const files = getEXRFiles(__dirname + '/public/textures/');
-    // console.log(files)
-    res.json(files)
-});
+  const files = getEXRFiles(__dirname + '/public/textures/')
+  res.json(files)
+})
 
-
-app.listen(3006, () => console.log('Visit http://127.0.0.1:3006'))
+// Start the server and then Python
+app.listen(3006, () => {
+  console.log('Visit http://127.0.0.1:3006')
+  ensureNiceguiInstalled()
+  startPythonApp()
+})
